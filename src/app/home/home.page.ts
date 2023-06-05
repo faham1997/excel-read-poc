c417a23c5fe8040a2aef5e4ce8486db3964f5f73
Nafid Faham <nafidfaham08@gmail.com>
2023-06-05 11:35:01 +0600

    Read file method implemented

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  public onFileSelected = (event: any) => {
+    const file: File = event.target.files[0];
+    this.readFile(file);
+  };
+
+  public readFile = (file: File) => {
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const data: Uint8Array = new Uint8Array(e.target.result);
+      const workbook: XLSX.WorkBook = XLSX.read(data, { type: 'array' });
+
+      workbook.SheetNames.forEach((page: string) => {
+        const worksheet: XLSX.WorkSheet = workbook.Sheets[page];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet, { raw: false });
+        console.log(`Page Name: ${page}`);
+        console.log(jsonData);
+      });
+    };
+    reader.readAsArrayBuffer(file);
+  };
 }
